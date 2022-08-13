4bb25798c5d61064f243f834d4ea86296228dc69
michiru <>
2022-08-13 16:34:33 +0900

    0812

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.Locale;
 import java.util.Map;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
@@ -24,36 +27,49 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SignupController {
 
-	@Autowired
-	private UserApplicationService userApplicationService;
+    @Autowired
+    private UserApplicationService userApplicationService;
 
-	/** ユーザ登録画面を表示 */
-	@GetMapping("/signup")
-	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
-		// 性別を取得
-		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
-		model.addAttribute("genderMap", genderMap);
+    @Autowired
+    private UserService userService;
 
-		// ユーザ登録画面に遷移
-		return "user/signup";
-	}
+    @Autowired
+    private ModelMapper modelMapper;
 
-	/** ユーザ登録処理 */
-	@PostMapping("/signup")
-	public String postSignup(Model model,Locale locale,
-			@ModelAttribute @Validated(GroupOrder.class) SignupForm form, 
-			BindingResult bindResult) {
+    /** ユーザー登録画面を表示 */
+    @GetMapping("/signup")
+    public String getSignup(Model model, Locale locale,
+            @ModelAttribute SignupForm form) {
+        // 性別を取得
+        Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
+        model.addAttribute("genderMap", genderMap);
 
-		//入力チェック
-		if(bindResult.hasErrors()) {
-			//NG:ユーザ登録画面に戻ります
-			return getSignup(model,locale,form);
-		}
-		
-		
-		log.info(form.toString());
+        // ユーザー登録画面に遷移
+        return "user/signup";
+    }
 
-		// ログイン画面にリセット
-		return "redirect:/login";
-	}
+    /** ユーザー登録処理 */
+    @PostMapping("/signup")
+    public String postSignup(Model model, Locale locale,
+            @ModelAttribute @Validated(GroupOrder.class) SignupForm form,
+
+            BindingResult bindingResult) {
+
+        // 入力チェック結果
+        if (bindingResult.hasErrors()) {
+            // NG:ユーザー登録画面に戻ります
+            return getSignup(model, locale, form);
+        }
+
+        log.info(form.toString());
+
+        // formをMUserクラスに変換
+        MUser user = modelMapper.map(form, MUser.class);
+
+        // ユーザー登録
+        userService.signup(user);
+
+        // ログイン画面にリダイレクト
+        return "redirect:/login";
+    }
 }
